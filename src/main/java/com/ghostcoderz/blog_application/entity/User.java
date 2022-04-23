b150c2409d49729e506a5f1e80c1fb8c67dbbc7e
GhostCoderz <vedasole001@gmail.com>
2022-04-23 12:20:15 +0530

    Updated Dependency

--- src/main/java/com/ghostcoderz/blog_application/entity/User.java
+++ src/main/java/com/ghostcoderz/blog_application/entity/User.java
@@ -3,11 +3,9 @@ package com.ghostcoderz.blog_application.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
-
 
 @Entity
 @Table(name="users")
