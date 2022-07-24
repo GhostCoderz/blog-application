87930f658262ac9fd49790db1802730e4643cfcb
GhostCoderz <vedasole001@gmail.com>
2022-07-24 20:32:17 +0530

    Updated for Heroku Deployment

--- src/main/java/com/ghostcoderz/blog_application/utils/AppRunningThread.java
+++ src/main/java/com/ghostcoderz/blog_application/utils/AppRunningThread.java
@@ -1,5 +1,7 @@
 package com.ghostcoderz.blog_application.utils;
 
+import com.ghostcoderz.blog_application.payload.PostDto;
+import org.springframework.web.client.RestTemplate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -13,7 +15,11 @@ public class AppRunningThread extends Thread{
             System.out.println("Current Time : " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("DD-MM-YYYY HH:MM:SS")));
             try {
                 //Wait for one min, so it doesn't print too fast
-                Thread.sleep(10000);
+                RestTemplate restTemplate = new RestTemplate();
+                restTemplate.getForObject("https://ghostarena.herokuapp.com/api/v1/posts/1",
+                        PostDto.class);
+                System.out.println("Post object retrieved!");
+                Thread.sleep(300000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
