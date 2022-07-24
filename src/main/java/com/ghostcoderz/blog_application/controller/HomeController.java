1b1ef2ea4026553d1e5a858ac04db64a013ed063
GhostCoderz <vedasole001@gmail.com>
2022-07-24 20:31:31 +0530

    Updated imports

--- src/main/java/com/ghostcoderz/blog_application/controller/HomeController.java
+++ src/main/java/com/ghostcoderz/blog_application/controller/HomeController.java
@@ -3,7 +3,6 @@ package com.ghostcoderz.blog_application.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
